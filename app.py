f2e2b700a4b4e0d286b838194c57100149605bda
jiqbal19 <jiqbal1901@gmail.com>
2026-07-30 11:49:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if not GEMINI_KEY or not BIN_ID or not JSONBIN_KEY:
     st.stop()
 
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Load Data from Cloud Database
 def load_cloud_data():
